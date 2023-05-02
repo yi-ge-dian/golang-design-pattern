f8c7b0603731f8567679ee0cd51fb74e8010b8ba
yi-ge-dian <1085266008@qq.com>
2023-05-02 15:33:25 +0800

    feat(structure type mode): 07_adaptor_pattern

---
+++ src/07_adaptor_pattern.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// Adaptee is the interface that needs to be adapted
+type Adaptee interface {
+	SpecificRequest() string
+}
+
+// AdapteeImpl is a concrete implementation of Adaptee
+type AdapteeImpl struct{}
+
+func (a *AdapteeImpl) SpecificRequest() string {
+	return "specific request"
+}
+
+// Target is the target interface that we want to adapt to.
+type Target interface {
+	Request() string
+}
+
+// Adapter is the adapter that adapts Adaptee to Target.
+type Adaptor struct {
+	Adaptee
+}
+
+func (a *Adaptor) Request() string {
+	return a.SpecificRequest()
+}
+
+// Client is the consumer of Target interface.
+func Client(t Target) string {
+	return t.Request()
+}
+
+func CallAdaptorPattern() {
+	adaptee := &AdapteeImpl{}
+	target := &Adaptor{Adaptee: adaptee}
+	output := Client(target)
+	fmt.Println(output)
+}
--- src/main.go
+++ src/main.go
@@ -1,11 +1,12 @@
 package main
 
 func main() {
-	//CallSinglePattern()          // 单例模式
-	//CallSampleFactoryPattern()   // 简单工厂模式
-	//CallAbstractFactoryPattern() // 抽象工厂模式
-	//CallBuilderPattern()         // 建造者模式
+	//CallSinglePattern()             // 单例模式
+	//CallSampleFactoryPattern()      // 简单工厂模式
+	//CallAbstractFactoryPattern()    // 抽象工厂模式
+	//CallBuilderPattern()            // 建造者模式
 	//CallConfigurePatternLowLevel()  // 建造者模式初级
 	//CallConfigurePatternHighLevel() // 建造者模式高级
-	CallPrototypePattern() // 原型模式
+	//CallPrototypePattern()          // 原型模式
+	//CallAdaptorPattern()            // 适配器模式
 }
