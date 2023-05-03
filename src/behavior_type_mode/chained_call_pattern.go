206bf5d1bea14679c11f4a6ff882a8454ca39a45
yi-ge-dian <1085266008@qq.com>
2023-05-03 17:08:53 +0800

    refactor(behavior type mode): 13_chained_call_pattern

---
+++ src/behavior_type_mode/chained_call_pattern.go
@@ -0,0 +1,30 @@
+package behavior_type_mode
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func NewPerson(name string, age int) *Person {
+	return &Person{name: name, age: age}
+}
+
+func (p *Person) SetName(name string) *Person {
+	p.name = name
+	return p
+}
+func (p *Person) SetAge(age int) *Person {
+	p.age = age
+	return p
+}
+
+func (p *Person) Describe() {
+	fmt.Printf("Name: %s, Age: %d\n", p.name, p.age)
+}
+
+func CallChainedCallPattern() {
+	person := NewPerson("John", 30)
+	person.SetName("Jack").SetAge(25).Describe()
+}
--- src/main.go
+++ src/main.go
@@ -1,19 +1,20 @@
 package main
 
-import "github.com/yi-ge-dian/golang-design-pattern/src/structure_type_mode"
+import "github.com/yi-ge-dian/golang-design-pattern/src/behavior_type_mode"
 
 func main() {
-	//CallSinglePattern()             // 单例模式
-	//CallSampleFactoryPattern()      // 简单工厂模式
-	//CallAbstractFactoryPattern()    // 抽象工厂模式
-	//CallBuilderPattern()            // 建造者模式
-	//CallConfigurePatternLowLevel()  // 建造者模式初级
-	//CallConfigurePatternHighLevel() // 建造者模式高级
-	//CallPrototypePattern()          // 原型模式
-	//CallAdaptorPattern()            // 适配器模式
-	//CallBridgePattern()             // 桥接模式
-	//CallObjectTreePattern()         // 对象树模式
-	//CallDecoratorPattern()          // 装饰器模式
-	//CallPipelinePattern()           // 管道模式
-	structure_type_mode.CallPluginPattern() // 插件模式
+	//creation_type_mode.CallSinglePattern()             // 单例模式
+	//creation_type_mode.CallSampleFactoryPattern()      // 简单工厂模式
+	//creation_type_mode.CallAbstractFactoryPattern()    // 抽象工厂模式
+	//creation_type_mode.CallBuilderPattern()            // 建造者模式
+	//creation_type_mode.CallConfigurePatternLowLevel()  // 建造者模式初级
+	//creation_type_mode.CallConfigurePatternHighLevel() // 建造者模式高级
+	//creation_type_mode.CallPrototypePattern()          // 原型模式
+	//structure_type_mode.CallAdaptorPattern()           // 适配器模式
+	//structure_type_mode.CallBridgePattern()            // 桥接模式
+	//structure_type_mode.CallObjectTreePattern()        // 对象树模式
+	//structure_type_mode.CallDecoratorPattern()         // 装饰器模式
+	//structure_type_mode.CallPipelinePattern()          // 管道模式
+	//structure_type_mode.CallPluginPattern()            // 插件模式
+	behavior_type_mode.CallChainedCallPattern()
 }
